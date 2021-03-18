2b22dc31c1dc5ab12235c6e53b579f7fdd13a6d3
Alex Miller <ajm9594@gmail.com>
2021-03-18 15:54:11 -0700

    global stylesheet

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,8 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        accessToken: "",
-        spaceId: "",
+        accessToken: "b-5v7OPfKG-9yfzoyog5nza8qY-SitmEmk2_0MO0Lhs",
+        spaceId: "flrl867lns2t",
       },
     },
   ],
