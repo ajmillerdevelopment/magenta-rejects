ff7263bcca04a2bb4a438a73da184bd8637f248f
Alex Miller <ajm9594@gmail.com>
2021-03-22 13:27:58 -0700

    media and index .js structure for merge

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,26 @@
 import * as React from "react"
+import logo from '../images/MRlogo-01.2984f31b.svg'
 
 // markup
 const IndexPage = () => {
   return (
-    <h1>Magenta Rejects</h1>
+    <>
+    <header>
+      <img src={logo} alt="Magenta Rejects Logo"/>
+      <div className="titlegroup">
+        <h1>MAGENTA REJECTS</h1>
+        <h2>...is an art collective</h2>
+      </div>
+    </header>
+    <nav>
+      <a href="/rebecca">rebecca</a>
+      <a href="/mars">mars</a>
+      <a href="/luxpacifica">lux pacifica</a>
+      <a href="/saribble"></a>
+      <a href="/collabs">collabs</a>
+      <a href="/shop">shop</a>
+    </nav>
+    </>
   )
 }
 
