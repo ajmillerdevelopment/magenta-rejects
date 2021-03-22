40966671913ffa69137cca62eff91cbcbd4b6050
Alex Miller <ajm9594@gmail.com>
2021-03-22 13:46:00 -0700

    index html skeleton

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import logo from '../images/logo.svg'
+import './global.css'
 
 // markup
 const IndexPage = () => {
@@ -9,17 +10,31 @@ const IndexPage = () => {
       <img src={logo} alt="Magenta Rejects Logo"/>
       <div className="titlegroup">
         <h1>MAGENTA REJECTS</h1>
-        <h2>...is an art collective</h2>
+        <p className="subhead">...is an art collective</p>
       </div>
     </header>
     <nav>
       <a href="/rebecca">rebecca</a>
       <a href="/mars">mars</a>
       <a href="/luxpacifica">lux pacifica</a>
-      <a href="/saribble"></a>
+      <a href="/saribble">s.a. ribble</a>
       <a href="/collabs">collabs</a>
       <a href="/shop">shop</a>
     </nav>
+    <main>
+      <section>
+        <h2>Shop</h2>
+        <div className="container">
+
+        </div>
+      </section>
+      <section>
+        <h2>Blog</h2>
+        <div className="container">
+          
+        </div>
+      </section>
+    </main>
     </>
   )
 }
