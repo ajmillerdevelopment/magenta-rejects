e9ce5c161d643381c46b267c62a3494899f98e36
Alex Miller <ajm9594@gmail.com>
2021-03-22 16:51:04 -0700

    switching

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,13 +26,17 @@ const IndexPage = () => {
       <section>
         <h2>Shop</h2>
         <div className="container">
-
+          <h3>See what's new from MR</h3>
+          <img src="https://picsum.photos/500/400" alt="placeholder"/>
+          <a href="/shop">Shop All</a>
         </div>
       </section>
       <section>
         <h2>Blog</h2>
         <div className="container">
-          
+          <h3>This is a blog post</h3>
+          <h4>By: Poppy</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dictumst proin orci quis interdum ultricies nibh. Mi euismod eu in nunc dui mauris placerat. Quam pretium nisi, et integer in pretium, sit felis elit. Dui in quam viverra libero ultrices nibh sed elementum leo. Hendrerit arcu aenean sed lacus, gravida ipsum. Sit turpis gravida laoreet sed nunc pellentesque. Fringilla pellentesque proin at sed lectus massa. Semper ut phasellus mattis posuere ullamcorper ultrices lorem. Ullamcorper commodo nulla pulvinar fames in id ut vestibulum, rhoncus. Quis suscipit convallis tortor venenatis donec metus elementum.</p>
         </div>
       </section>
     </main>
